b28d728b421b54b6deb26a3cf30f242ae4a2458a
Daakni <daakni@gmail.com>
2019-12-01 12:20:37 +0100

    Setup django with docker

--- 2dolist/app/models/task.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Task struct {
-	Name string ` json:"name" xml:"name" `
-	Done bool ` json:"done"`
-	Date string ` json:"date" xml:"date" `
-	DueDate string ` json:"dueDate" xml:"dueDate" `
-}
